3b2c0d175b9323932ae69fb9df0246f19251407e
kingtony20 <tony_villanueva20@outlook.com>
2026-03-16 16:20:48 -0500

    Actualización del sistema Login-Marcadores

---
+++ app/gerente/dashboard/page.tsx
@@ -0,0 +1,9 @@
+// app/gerente/dashboard/page.tsx
+export default function DashboardPage() {
+    return (
+      <div>
+        <h1>Dashboard del Gerente</h1>
+        {/* ... */}
+      </div>
+    )
+  }
